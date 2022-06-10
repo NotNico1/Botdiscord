c7a4f352a3709b675ca2ac44873972bcb18c6364
NotNico1 <89837736+NotNico1@users.noreply.github.com>
2022-06-10 10:18:55 +0200

    Upload files

---
+++ index.js
@@ -0,0 +1,49 @@
+const Discord = require("discord.js")
+const client = new Discord.Client(
+    { intents: ["GUILDS", "GUILD_MEMBERS", "GUILD_BANS", "GUILD_EMOJIS_AND_STICKERS", "GUILD_INTEGRATIONS", "GUILD_WEBHOOKS", "GUILD_INVITES", "GUILD_VOICE_STATES", "GUILD_PRESENCES", "GUILD_MESSAGES", "GUILD_MESSAGE_REACTIONS", "GUILD_MESSAGE_TYPING", "DIRECT_MESSAGES", "DIRECT_MESSAGE_REACTIONS", "DIRECT_MESSAGE_TYPING", "GUILD_SCHEDULED_EVENTS"] }
+)
+
+client.login("OTg0Mzg0MDE3NTQzNjA2Mjcy.G6SiJi.Wqi58RhHQOOfF12pUAvZs0s8l531meiiIqtizo")
+
+client.on("ready", () => {
+    console.log("Bot ONLINE")
+})
+
+client.on("messageCreate", (message) => {
+    if (message.content == "!instagram") {
+        console.log("Command !instagram Success Client")
+        message.channel.send("Instagram ufficiale: https://instagram.com/eldianmc?igshid=YmMyMTA2M2Y=")
+        console.log("Command !instagram Success Server")
+   }
+
+    if (message.content == "!tiktok") {
+        console.log("Command !instagram Success Client")
+        message.channel.send("TikTok ufficiale: https://www.tiktok.com/@eldianmc")
+        console.log("Command !instagram Success Server")
+    }
+
+    if (message.content == "eb") {
+    console.log("Command eb Success from Client")
+        message.channel.send("Come posso aiutarti?")
+    console.log("Command eb Success from Server")
+    
+    }
+
+    if (message.content == "eb come stai?") {
+        console.log("Command eb Success from Client")
+        message.channel.send("Bene e tu?")
+        console.log("Command !instagram Success Server")
+
+    }
+
+    if (message.content == "!ip") {
+        console.log("Command eb Success from Client")
+        var embed = new Discord.MessageEmbed()
+            .setTitle("Eldian")
+            .setDescription("IP: mc.eldian.it")
+            .setThumbnail("https://cdn.discordapp.com/attachments/825804924055060480/983051751584333854/Eldian.png")
+
+        message.channel.send({ embeds: [embed] })
+        console.log("Command !instagram Success Server")
+    } 
+})
